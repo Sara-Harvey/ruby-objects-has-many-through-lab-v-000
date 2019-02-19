d10546e839dab5a83789b80e599a5858b1645261
Sara Harvey <saraharveynyc@gmail.com>
2019-02-19 16:46:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,24 @@
-class Patient 
-  
-@@all = []
-  
-def initialize
-end
+class Patient
 
-def all 
-  @@all
-end
+   attr_reader :name, :appointment, :doctor
 
-end 
+   def initialize(name)
+    @name = name
+  end
+
+   def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+   def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+   def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
+  end
+end
